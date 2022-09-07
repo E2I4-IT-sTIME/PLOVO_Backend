6c5718a71ea922d34be6b77f2e9dbdf754e8a045
ChaeyeonHan <kate2236e@naver.com>
2022-09-08 01:39:47 +0900

    feat : Add User, SwaggerConfig

---
+++ src/main/java/com/kb_hackathon/plovo/domain/User.java
@@ -0,0 +1,34 @@
+package com.kb_hackathon.plovo.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Builder
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "USER_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String k_username;
+
+    @Column(nullable = false)
+    private String username;
+
+    private String image;
+
+    private Boolean status;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+}
