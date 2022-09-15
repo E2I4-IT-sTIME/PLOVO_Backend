286829070bc8c2c99ba009dbddac6e6b8a0caed1
kyj <khkkgon0118@gmail.com>
2022-09-15 22:55:11 +0900

    feat : qlrm 홈화면 api

--- src/main/java/com/kb_hackathon/plovo/repository/MountainRepository.java
+++ src/main/java/com/kb_hackathon/plovo/repository/MountainRepository.java
@@ -8,10 +8,6 @@ import java.util.List;
 
 public interface MountainRepository extends JpaRepository<Mountain, Long> {
 
-    @Query(value = "SELECT m.m_name, m.main_img, p.weight FROM mountain m left join plovo p on m.id = p.mountain_id " +
-            "ORDER BY p.weight DESC LIMIT 5", nativeQuery = true)
-    List<Mountain> mRecommend();
-
     @Query(value = "SELECT m.m_name, m.main_img, p.weight FROM mountain m left outer join plovo p on m.id = p.mountain_id WHERE m.m_name like '%m.m_name = :mName%'", nativeQuery = true)
     List<Mountain> mfindBymName(String mName);
 
