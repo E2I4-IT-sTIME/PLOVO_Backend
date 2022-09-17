e5309ac8c2981f0a7f702516da7d25edda169833
kyj <khkkgon0118@gmail.com>
2022-09-18 04:01:37 +0900

    feat : 소셜 차트 절댓값 수정

--- src/main/java/com/kb_hackathon/plovo/dto/UserUploadImgRes.java
+++ src/main/java/com/kb_hackathon/plovo/dto/UserUploadImgRes.java
@@ -4,9 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserUploadImgRes {
     private String uploadImg;
+    private Timestamp date;
+    private String distance;
+    private String time;
+    private String weight;
+    private String mName;
+    private String profileImg;
 }
